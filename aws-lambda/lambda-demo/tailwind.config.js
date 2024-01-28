38f4b6eefe99ba5952e890fc2b3bf52ff0ef853e
newbmiao <newbvirgil@gmail.com>
2024-01-28 20:24:36 +0800

    add tailwindcss

---
+++ aws-lambda/lambda-demo/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/*.rs',
+    './templates/**/*.html',
+    './public/**/*.{html,js,css}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
+
